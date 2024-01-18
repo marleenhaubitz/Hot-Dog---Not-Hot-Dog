610c2a68e7d35896bc6cd7b46b270bcaf4f71fd6
RWichhart15 <51769719+RWichhart15@users.noreply.github.com>
2024-01-18 14:01:49 +0100

    function test

--- hello.py
+++ hello.py
@@ -4,3 +4,9 @@ print("why does this not work")
 print("changes marleen")
 
 print("testing 1")
+
+import hotdog as hot
+
+hot.addnum(1,5)
+
+
--- hotdog.py
+++ hotdog.py
@@ -1,6 +1,9 @@
 # Neural Net Hotdog Image Recognition
 
 
+def addnum(num1, num2):
+    return num1 + num2
+
 
 
 

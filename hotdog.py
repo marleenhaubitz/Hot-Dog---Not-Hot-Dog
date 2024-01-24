c54cbe45823f34a14a5fb68a01470f5cee580f5b
RWichhart15 <51769719+RWichhart15@users.noreply.github.com>
2024-01-24 13:35:34 +0100

    Setup and Installation
    I followed the keras docs to setup the environment and import the modules.

--- hotdog.py
+++ hotdog.py
@@ -1,11 +1,19 @@
 # Neural Net Hotdog Image Recognition
 
+# Neural net docs 
+# Installing Tensorflow is necessary, apparently. 
+# https://keras.io/api/ 
+# https://keras.io/getting_started/intro_to_keras_for_engineers/
 
-def addnum(num1, num2):
-    return(num1 + num2)
+import tensorflow as tf
+import numpy as np
+import os
+print("Keras version is: " + keras.__version__)
 
 
+os.environ["KERAS_BACKEND"] = "tensorflow"
 
+import keras
 
 
 

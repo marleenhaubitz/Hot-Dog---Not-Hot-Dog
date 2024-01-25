d8cb9b5a53faeda761b94408793914427a5763ab
marleenhaubitz <131541048+marleenhaubitz@users.noreply.github.com>
2024-01-25 12:57:47 +0100

    Update hotdog.py

--- hotdog.py
+++ hotdog.py
@@ -39,6 +39,18 @@ for images, labels in train_ds.take(1):
 
 plt.show() 
 
+images_2 = plt.figure(figsize=(10, 10))
+for images, labels in test_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(np.array(images[i]).astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+
+plt.show() 
+
+
+
 
 
 

09cbf93acb3cda270a14586151e18d4d1f5a3961
RWichhart15 <51769719+RWichhart15@users.noreply.github.com>
2024-01-25 12:49:53 +0100

    its hotdogging

--- hotdog.py
+++ hotdog.py
@@ -13,24 +13,24 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 import keras
 print("Keras version is: " + keras.__version__)
 
-directory = "archive"
+directory = "archive/train"
 
-hotdog_images = keras.utils.image_dataset_from_directory(
+train_ds = keras.utils.image_dataset_from_directory(
     directory,
     labels="inferred",
     label_mode="int",
 )
 
-print(hotdog_images)
 
-plt.figure(figsize=(10, 10))
-for images, labels in hotdog_images.take(1):
+images_1 = plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
     for i in range(9):
         ax = plt.subplot(3, 3, i + 1)
         plt.imshow(np.array(images[i]).astype("uint8"))
         plt.title(int(labels[i]))
         plt.axis("off")
 
+plt.show()
 
 
 
